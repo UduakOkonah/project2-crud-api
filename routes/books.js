a9a01fd2397c4196e26ec88e1158d5fd6222553b
UduakOkonah <uduakobongokonah@gmail.com>
2025-09-25 09:15:25 +0100

    Initial commit - Project 2 CRUD API

---
+++ routes/books.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const router = express.Router();
+const booksController = require("../controllers/booksController");
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: Book management
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - isbn
+ *         - publishedDate
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Auto-generated ID
+ *         title:
+ *           type: string
+ *           description: Book title
+ *         author:
+ *           type: string
+ *           description: Author of the book
+ *         isbn:
+ *           type: string
+ *           description: Unique ISBN
+ *         publishedDate:
+ *           type: string
+ *           format: date
+ *           description: Date the book was published (YYYY-MM-DD)
+ *       example:
+ *         title: The Great Gatsby
+ *         author: F. Scott Fitzgerald
+ *         isbn: "123-456789"
+ *         publishedDate: "1925-04-10"
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: List of books
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       201:
+ *         description: Book created successfully
+ *
+ * /books/{id}:
+ *   get:
+ *     summary: Get a book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book found
+ *   put:
+ *     summary: Update a book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: Book updated
+ *   delete:
+ *     summary: Delete a book by ID
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book deleted
+ */
+
+router.get("/", booksController.getBooks);
+router.get("/:id", booksController.getBook);
+router.post("/", booksController.createBook);
+router.put("/:id", booksController.updateBook);
+router.delete("/:id", booksController.deleteBook);
+
+module.exports = router;
